e624e90e0869fdbbae5a938ce6abc361276bb477
dodo7 <dodo7@192.168.219.101>
2023-04-19 18:53:00 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	public static void main(String[] orgs) {
+		System.out.println("Hello World\n");
+		System.out.println("\t\\Hello World/\n\n");
+		System.out.println("\"Hello World!\"");
+		
+		System.out.printf("%d + %d = %d", 10,20, (10+20));
+	}
+}
+ 
